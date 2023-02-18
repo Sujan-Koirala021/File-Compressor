f6c41f372db8ed803105b538308478e964a05cbb
Sujan Koirala <077bct087.sujan@pcamupus.edu.np>
2023-02-18 19:01:50 +0545

    Fetches file name from filedialog

--- app_ui.py
+++ app_ui.py
@@ -1,6 +1,15 @@
 from tkinter import *
-root = Tk()
-root.title("File Compressor")
-root.iconbitmap("imgs/app_icon.ico")
+from tkinter import filedialog
 
-root.mainloop()
+win = Tk()
+win.title("File Compressor")
+win.iconbitmap("imgs/app_icon.ico")
+
+#   Set window size
+win.geometry("600x400")
+
+
+win.filename = filedialog.askopenfilename(initialdir="C:/Users/user/Downloads", title = "Select A File", filetypes=(("text files", "*.txt"), ("all files", "*.*")))
+print(win.filename)
+
+win.mainloop()
