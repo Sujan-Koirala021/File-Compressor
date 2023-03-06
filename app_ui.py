97f6b58cf1301c51371035d07874424af902ac7d
Sujan Koirala <077bct087.sujan@pcamupus.edu.np>
2023-03-06 13:29:44 +0545

    Add home img

--- app_ui.py
+++ app_ui.py
@@ -5,6 +5,8 @@ import customtkinter
 
 width = 600
 height= 400
+lightThemeBg = "#26242f"
+darkThemeBg = "#dee4e7"
 
 win = Tk()
 win.title("File Compressor")
@@ -13,26 +15,51 @@ win.iconbitmap("imgs/app_icon.ico")
 #   Set window size
 
 win.geometry("{}x{}".format(width, height)) # same as win.geometry("600x400")
-win.config(bg = "#dee4e7")
+win.config(bg = darkThemeBg)
 
 lightState = PhotoImage(file="imgs/light.png")
 darkState = PhotoImage(file="imgs/dark.png")
 
+
+
 lightMode = True
+
+
+canvas = Canvas(win, width=300,bd=0, highlightthickness=0, relief='ridge', height=300, bg = darkThemeBg)
+canvas.place(x = 50,y =  100)
+homeImg = PhotoImage(file="imgs/home_img.png").subsample(2, 2)
+
+
+# Create the image on the canvas without a box-like background
+canvas.create_image(0, 0, anchor="nw", image=homeImg)
+
+
+
+
 
 def switchMode():
     global lightMode
     if lightMode:
-        button.config(image = darkState, bg = "#26242f", activebackground="#26242f")
-        win.config(bg = "#26242f")
+        button.config(image = darkState, bg = lightThemeBg, activebackground=lightThemeBg)
+        canvas.config(bg = lightThemeBg)
+
+        win.config(bg = lightThemeBg)
         lightMode = False
     else:
-        button.config(image = lightState, bg = "#dee4e7", activebackground="#dee4e7")
-        win.config(bg = "#dee4e7")
+        button.config(image = lightState, bg = darkThemeBg, activebackground=darkThemeBg)
+        canvas.config(bg = darkThemeBg)
+
+        win.config(bg = darkThemeBg)
         lightMode = True
     
 
-button = Button(win, image=lightState, bd = 0, bg = "#dee4e7", activebackground="#dee4e7", command = switchMode)
+
+
+
+
+
+
+button = Button(win, image=lightState, bd = 0, bg = darkThemeBg, activebackground=darkThemeBg, command = switchMode)
 button.pack(padx=50, pady = 50)
 button.place(x = width - 150, y = 10)
 
