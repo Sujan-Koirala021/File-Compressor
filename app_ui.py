c6a5c54b083f9087d9c95fb5cbb1e4277eaba9ed
Sujan Koirala <077bct087.sujan@pcamupus.edu.np>
2023-03-06 14:17:19 +0545

    good looks button

--- app_ui.py
+++ app_ui.py
@@ -2,6 +2,7 @@ import os
 from tkinter import *
 from tkinter import filedialog
 import customtkinter
+import tkinter.font as font
 
 width = 600
 height= 400
@@ -23,6 +24,7 @@ darkState = PhotoImage(file="imgs/dark.png")
 
 
 lightMode = True
+buttonFont = font.Font(family='Helvetica', size=18, weight='bold')
 
 
 canvas = Canvas(win, width=300,bd=0, highlightthickness=0, relief='ridge', height=300, bg = darkThemeBg)
@@ -32,7 +34,6 @@ homeImg = PhotoImage(file="imgs/home_img.png").subsample(2, 2)
 
 # Create the image on the canvas without a box-like background
 canvas.create_image(0, 0, anchor="nw", image=homeImg)
-
 
 
 
@@ -59,7 +60,7 @@ def switchMode():
 
 
 
-button = Button(win, image=lightState, bd = 0, bg = darkThemeBg, activebackground=darkThemeBg, command = switchMode)
+button = Button(win, image=lightState, bd = 0, bg = darkThemeBg, activebackground=darkThemeBg,font = buttonFont, command = switchMode)
 button.pack(padx=50, pady = 50)
 button.place(x = width - 150, y = 10)
 
@@ -80,16 +81,15 @@ def openFileDialogForExtraction():
 
 
 # Use CTkButton instead of tkinter Button
-compressButton = customtkinter.CTkButton(master=win, text="Compress file",hover=True, hover_color="#0b6eca", width=150,height=40,border_width=0,corner_radius=0,command=openFileDialogForCompression)
+compressButton = customtkinter.CTkButton(master=win, text_color=("black", "black"), text="Compress file",hover=True, hover_color="#0b6eca", width=170,height=50,border_width=0,corner_radius=0,text_font=("Courier", 12), command=openFileDialogForCompression)
 compressButton.place(x = 3*width/4 , y =height/2 - 30, anchor=CENTER)
-
 
 # extractButton = Button(win, text = "Extract File", command = openFileDialogForExtraction)
 # extractButton.place(x = 3*width/4 , y =height/2 + 50)
 
 
 # Use CTkButton instead of tkinter Button
-extractButton = customtkinter.CTkButton(master=win, text="Extract file",hover=True, hover_color="#0b6eca", width=150,height=40,border_width=0,corner_radius=0, command=openFileDialogForExtraction)
+extractButton = customtkinter.CTkButton(master=win, text="Extract file",text_color=("black", "black"),hover=True, hover_color="#0b6eca", width=170,height=50,border_width=0,corner_radius=0,text_font=("Courier", 12), command=openFileDialogForExtraction)
 extractButton.place(x = 3*width/4 , y =height/2 + 50, anchor=CENTER)
 
 win.mainloop()
