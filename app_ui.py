fde4dfb9418783df92775941ef9758872e4c4283
Sujan Koirala <077bct087.sujan@pcamupus.edu.np>
2023-03-06 10:11:38 +0545

    Add hover effect

--- app_ui.py
+++ app_ui.py
@@ -13,7 +13,7 @@ win.iconbitmap("imgs/app_icon.ico")
 #   Set window size
 
 win.geometry("{}x{}".format(width, height)) # same as win.geometry("600x400")
-win.config(bg = "white")
+win.config(bg = "#dee4e7")
 
 lightState = PhotoImage(file="imgs/light.png")
 darkState = PhotoImage(file="imgs/dark.png")
@@ -27,12 +27,12 @@ def switchMode():
         win.config(bg = "#26242f")
         lightMode = False
     else:
-        button.config(image = lightState, bg = "white", activebackground="white")
-        win.config(bg = "white")
+        button.config(image = lightState, bg = "#dee4e7", activebackground="#dee4e7")
+        win.config(bg = "#dee4e7")
         lightMode = True
     
 
-button = Button(win, image=lightState, bd = 0, bg = "white", activebackground="white", command = switchMode)
+button = Button(win, image=lightState, bd = 0, bg = "#dee4e7", activebackground="#dee4e7", command = switchMode)
 button.pack(padx=50, pady = 50)
 button.place(x = width - 150, y = 10)
 
@@ -53,15 +53,16 @@ def openFileDialogForExtraction():
 
 
 # Use CTkButton instead of tkinter Button
-compressButton = customtkinter.CTkButton(master=win, text="Compress file", command=openFileDialogForCompression)
+compressButton = customtkinter.CTkButton(master=win, text="Compress file",hover=True, hover_color="#0b6eca", width=150,height=40,border_width=0,corner_radius=0,command=openFileDialogForCompression)
 compressButton.place(x = 3*width/4 , y =height/2 - 30, anchor=CENTER)
 
 
 # extractButton = Button(win, text = "Extract File", command = openFileDialogForExtraction)
 # extractButton.place(x = 3*width/4 , y =height/2 + 50)
 
+
 # Use CTkButton instead of tkinter Button
-extractButton = customtkinter.CTkButton(master=win, text="Extract file", command=openFileDialogForExtraction)
+extractButton = customtkinter.CTkButton(master=win, text="Extract file",hover=True, hover_color="#0b6eca", width=150,height=40,border_width=0,corner_radius=0, command=openFileDialogForExtraction)
 extractButton.place(x = 3*width/4 , y =height/2 + 50, anchor=CENTER)
 
 win.mainloop()
